5ccd70fd368b72fad7f6c41bc521be480352cd97
parth7222 <parththesiya@gmail.com>
2020-07-08 10:28:49 +0530

    update V0.0.1

--- src/Valve-test.ino
+++ src/Valve-test.ino
@@ -1,8 +1,47 @@
 #include "Arduino.h"
 
+/*
+Details of 
+where to use valve 
+what use of valve
+Liquid 0r Gas
+Feedback system
+etc
+every littel details about valve use
+*/
+
+//single valve 
+#define VALVE_PIN 2
+
+//duble valve
+#define VALVE_PIN_1 2
+#define VALVE_PIN_1 3
+
+//sensor type for valve Feedback
+/* 
+you can use this type of sensor
+: temprature sensor 
+: flow sensor
+: presser sensor
+: flame sensor
+*/
+
+#define TEMPRATURE_SENSOR 4
+#define FLOW_SENSOR 4
+#define PRESSUER_SENSOR 4
+#define FLAME_SENSOR 4
+
 void setup()
 {
+    pinMode(VALVE_PIN,OUTPUT);
 
+    pinMode(VALVE_PIN_1,OUTPUT);
+    pinMode(VALVE_PIN_2,OUTPUT);
+
+    pinMode(TEMPRATURE_SENSOR,INPUT);
+    pinMode(FLOW_SENSOR,INPUT);
+    pinMode(PRESSUER_SENSOR,INPUT);
+    pinMode(FLAME_SENSOR,INPUT);
 }
 
 void loop()
