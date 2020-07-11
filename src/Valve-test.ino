5dae5b0c9cb64e55cf9bad877d6e49d6a8a2e755
parth7222 <parththesiya@gmail.com>
2020-07-11 15:18:47 +0530

    new update

--- src/Valve-test.ino
+++ src/Valve-test.ino
@@ -8,53 +8,102 @@ Liquid 0r Gas
 Feedback system
 etc
 every littel details about valve use
+
+this code file is only for single feedback sensor 
+you can modify if know ablut multi feedback
+
+there is no responsibilty for your hardware 
+so change when you know this not damage your hardware 
+
 */
 
-// #define CONSTANT 
-// #define NON_CONSTANT
 
 //select onlyone valve system
+
 #define VALVE_SINGLE true
+
 //#define VALVE_DOUBLE true
+
 //#define MOTOR_STEPPER true
+
 //#define MOTOR_SERVO_MINI true
+
 //#define MOTOR_DC true
 
-//select sensor system
-#define TEMPRATURE_SENSOR true
-//#define FLOW_SENSOR true
-//#define PRESSUER_SENSOR true
-//#define FLAME_SENSOR true
 
-//single valve 
-#if VALVE_SINGLE
-#define NON_CONSTANT
-#define VALVE_PIN 2
-#elif VALVE_DOUBLE
-//duble valve
-#define CONSTANT 
-#define VALVE_PIN_1 2
-#define VALVE_PIN_1 3
-#elif MOTOR_STEPPER
-#define CONSTANT 
-#define STEP_PIN 2
-#define DIRC_PIN 3
-#elif MOTOR_SERVO_MINI
-#define CONSTANT 
-#define PULSE_PIN 2
-#elif MOTOR_DC
-#define CONSTANT 
-#define STEP_PIN 2
-#define DIRC_PIN 3
+
+//select sensor type
+
+#define ANALOG_SENSOR 1      //sensor output is analog then select this type sensor 
+
+//#define DIGITAL_SENSOR 1   //sensor output is digital then select this type sensor 
+
+
+
+#if ANALOG_SENSOR
+    #define ANALOG_INPUT A0//select pin for analog sensor input
+    #define INPUT_VALUE ANALOG_INPUT
+#elif DIGITAL_SENSOR 
+    #define DIGITAL_INPUT 1
+    #define DIGITAL_MIN_INPUT 1   //value in digits so, dont change until you know ablut your sensor default value
+    #define DIGITAL_MAX_INPUT 100
+    #define INPUT_VALUE DIGITAL_INPUT
 #endif
 
-#define ANALOG_INPUT A0//use for single pin analog input sensor
+
+/*
+    which type valve system you want to use 
+    and there is CONSTANT & NON_CONSTANT system
+    which look at your valve application
+    so take care of that things 
+    so be carefull when you use skech like this
+*/
+//single valve 
+#if VALVE_SINGLE
+    #define NON_CONSTANT 1
+    #define VALVE_PIN 2
+#elif VALVE_DOUBLE
+//duble valve
+    #define CONSTANT 1
+    #define VALVE_PIN_1 2
+    #define VALVE_PIN_1 3
+#elif MOTOR_STEPPER
+    #define CONSTANT 1
+    #define STEP_PIN 2
+    #define DIRC_PIN 3
+#elif MOTOR_SERVO_MINI
+    #define CONSTANT 1
+    #define PULSE_PIN 2
+#elif MOTOR_DC
+    #define CONSTANT 1
+    #define STEP_PIN 2
+    #define DIRC_PIN 3
+#endif
+
+
+
+//do not change this value C'oz this work on which type valve system
+//you selected
+#if NON_CONSTANT
+    #define RELAY_TYPE 1
+#elif CONSTANT
+    #define MIN_VALUE 0
+    #define MAX_VALUE 100
+#endif
+
+
+
+//#define ANALOG_INPUT A0//use for single pin analog input sensor
+
+
 
 //variable define here 
 double kp = 2;
 double ki = 5;
 double kd = 1;
  
+
+
 unsigned long currentTime, previousTime;
 double elapsedTime;
 double error;
@@ -62,59 +111,71 @@ double lastError;
 double input, output, setPoint;
 double cumError, rateError;
 
+
+
 //details about PID 
 //setPoint = 0;
 int sensor_value = 0;
 
-//sensor type for valve Feedback
-/* 
-you can use this type of sensor
-: temprature sensor 
-: flow sensor
-: presser sensor
-: flame sensor
-*/
-#if TEMPRATURE_SENSOR
-#define TEMPRATURE_SENSOR
-#elif FLOW_SENSOR
-#define FLOW_SENSOR 
-#elif PRESSUER_SENSOR
-#define PRESSUER_SENSOR 
-#elif FLAME_SENSOR
-#define FLAME_SENSOR 
-#endif
+
 
 void setup()
 {   
     setPoint = 0;
 
     #if VALVE_SINGLE
-    pinMode(VALVE_PIN,OUTPUT);
+        pinMode(VALVE_PIN,OUTPUT);
     #elif VALVE_DOUBLE
-    pinMode(VALVE_PIN_1,OUTPUT);
-    pinMode(VALVE_PIN_2,OUTPUT);
+        pinMode(VALVE_PIN_1,OUTPUT);
+        pinMode(VALVE_PIN_2,OUTPUT);
     #elif VALVE_DOUBLE
-    pinMode(STEP_PIN,OUTPUT);
-    pinMode(DIRC_PIN,OUTPUT);
+        pinMode(STEP_PIN,OUTPUT);
+        pinMode(DIRC_PIN,OUTPUT);
     #elif
-    pinMode(PULSE_PIN,OUTPUT);
+        pinMode(PULSE_PIN,OUTPUT);
     #elif VALVE_DOUBLE
-    pinMode(STEP_PIN,OUTPUT);
-    pinMode(DIRC_PIN,OUTPUT);
+        pinMode(STEP_PIN,OUTPUT);
+        pinMode(DIRC_PIN,OUTPUT);
     #endif
 
     
 }
 
+
+
 void loop()
 {
-    input = analogRead(ANALOG_INPUT);                //read from selected sensor system
+    input_value();                //read from selected sensor system
     output = computePID(input);            //calculate PID using selected system configuration 
     delay(100);                            //gap between two loop
 
 
     analogWrite(3, output);                //control the valve based on PID value or motors
 }
+
+
+
+void input_value()
+{
+    #if ANALOG_SENSOR
+        input = analogRead(INPUT_VALUE);
+    #elif DIGITAL_SENSOR
+        input = digitalCalibration();
+    #endif
+}
+
+
+
+int digitalCalibration()
+{
+    #define MAX_ANALOG 1023
+    #ifdef DIGITAL_SENSOR
+        INPUT_VALUE = ((DIGITAL_MAX_INPUT * DIGITAL_INPUT) / MAX_ANALOG);
+        return INPUT_VALUE; 
+    #endif
+}
+
+
 
 double computePID(double inp)
 {     
